5b6bfe399665f6308120b4d9b1a916f1c58c0726
bhillmann <hillmannben@gmail.com>
2020-05-24 15:50:53 -0500

    first steps for parsing file complete

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ env :
 
 up : env build
 	docker-compose up -d --force-rebuild
+
+update-env :
+	pip install --no-cache-dir -r ./requirements.txt --upgrade
--- app/main.py
+++ app/main.py
@@ -1,18 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import requests
 import uvicorn
 
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+def parse_database():
+    database_link = "https://raw.githubusercontent.com/Manuel-777/MTG-Arena-Tool/master/src/resources/database.json"
+    response = requests.get(database_link)
+    return response.json()['cards']
+
+
+db = parse_database()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{id}")
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    for line in file.read():
+        print(line)
+    return {"filename": file.filename}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info", reload=True)
